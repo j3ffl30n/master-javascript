254d618969596c8bc7fba5475ab4d7d9321b405d
Jeff <jeffrey.leon>
2018-06-26 01:32:17 -0400

    Ejercicio 2

---
+++ scripts/Ejercicio-01.js
@@ -0,0 +1,31 @@
+'use strict'
+
+
+var nombre = 'jeff'
+console.log(nombre)
+
+var confirmacion = confirm("Yes or N");
+console.log(confirmacion);
+
+var edad = prompt('Introduce un numero', 0);
+//Para verificar si no es un numero
+console.log(isNaN(edad));
+
+var edad = parseInt(prompt('Introduce un numero', 0));
+console.log(edad);
+var imprimir = "";
+switch(edad){
+	case 1: 
+		imprimir = "Elegiste la opcion 1";
+	break;
+	case 2: 
+		imprimir = "Elegiste la opcion 2";
+	break;
+	case 1: 
+		imprimir = "Elegiste la opcion 3";
+	break;
+	default: 
+		imprimir = "No se selecciono opcion, se asignara 0"
+	break;
+}
+console.log(imprimir);
